6fb34f0e728cf924c3a600f8aae927a41f54187c
gamok555 <gamok555@naver.com>
2025-05-01 15:37:10 +0900

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,1 +1,38 @@
+import javax.swing.*;          // JFrame, JButton, JOptionPane …
+import java.awt.*;             // BorderLayout, FlowLayout …
+import java.awt.event.*;       // ActionListener …
+import java.util.Random;
 
+public class UI{
+
+  //map 변수 4는 4각형 5는 5각형 6은 6각형
+  public void gamesetup(int map, int player, int token){
+    
+  }
+
+  //말이동으로 어떤말이 어떻게 움직였는지만 받아오면 가능
+  public void mapupdate(){
+    
+  }
+
+  //return 0 means random button click, return 1 means select button click
+  public int throwing(){
+    
+  }
+
+  //return 0~5 빽도 도 개 걸 윷 모
+  public int choice_yut(){
+    
+  }
+
+  // return chosen horse number    start from 1
+  public int choice_horse(){
+    
+  }
+
+  // if restart button pressed return 1; end button return 0
+  public int end(){
+    
+  }
+  
+}
