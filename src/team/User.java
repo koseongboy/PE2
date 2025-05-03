2d05143da7a6004442bc269b13a1f3d0b17e56f6
koseongboy <koseonghyeon08@gmail.com>
2025-05-03 18:14:58 +0900

    유져 생성자 구현

---
+++ src/team/User.java
@@ -0,0 +1,25 @@
+package team;
+
+import java.util.ArrayList;
+
+public class User implements Team_Interface {
+	private ArrayList<Horse> horses;
+	
+	
+	public User(int num_of_horses) {
+		for (int i = 0; i < num_of_horses; i++) {
+			Horse horse = new Horse();
+			this.horses.add(horse)
+		}
+	}
+	@Override
+	public ArrayList<Horse> getHorses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+		public boolean moveHorse(Horse horse, int count) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+}
